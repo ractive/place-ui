9f056a3ced25d63f15b641b996239641f4b4ed19
james <james@ractive.ch>
2019-11-05 00:27:14 +0100

    Render opening hours

--- pages/places/[id].tsx
+++ pages/places/[id].tsx
@@ -4,10 +4,57 @@ import Layout from '../../components/Layout';
 import fetch from 'isomorphic-unfetch';
 import * as React from "react";
 
+type LocalTimeRange = {
+    start: string;
+    end: string;
+};
+
+type OpeningHoursRange = {
+    weekdayStart: string;
+    weekdayEnd: string;
+
+    localTimeRanges: LocalTimeRange[];
+};
+
+const OpenRanges = ({localTimeRanges} : {localTimeRanges: LocalTimeRange[]}) => {
+    return <div>
+        {
+            localTimeRanges.map(range => <div>{range.start + " - " + range.end}</div>)
+        }
+    </div>;
+};
+
+const Weekdays = ({start, end} : {start: string, end: string}) => {
+    if (start === end) {
+        return <div>{start}</div>
+    } else {
+        return <div>{start + " - " + end}</div>
+    }
+};
+
+interface IOpeningHoursProps {
+    openingHoursRanges: OpeningHoursRange[];
+}
+const OpeningHours = (props: IOpeningHoursProps) => {
+  return <div>
+      <b>Opening Hours</b>
+      {
+          props.openingHoursRanges.map(range => {
+              return <div className="opening-hours">
+                  <Weekdays start={range.weekdayStart} end={range.weekdayEnd}/>
+                  <OpenRanges localTimeRanges={range.localTimeRanges}/>
+              </div>
+              }
+          )
+      }
+  </div>;
+};
+
 interface IPlaceProps {
     data?: {
         name: string;
         address: string;
+        openingHoursRanges: OpeningHoursRange[];
     },
     error?: {
         statusCode
@@ -26,7 +73,7 @@ const Place: NextPage<IPlaceProps> = ({error, data}) => {
                         <p>{data.address}</p>
                     </div>
                     <div>
-                        <b>Opening hours</b>
+                        <OpeningHours openingHoursRanges={data.openingHoursRanges}/>
                     </div>
                 </div>
                 <style jsx>{`
@@ -52,7 +99,8 @@ Place.getInitialProps = async ({ query }) => {
         return {
             data: {
                 name: data.name,
-                address: data.address
+                address: data.address,
+                openingHoursRanges: data.openingHoursRanges
             }
         };
     } else {
