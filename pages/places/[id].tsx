85b74518ebaf56e64536feba071e8be698f0f83b
james <james@ractive.ch>
2019-11-04 23:50:06 +0100

    Load place from backend and display name and address

--- pages/places/[id].tsx
+++ pages/places/[id].tsx
@@ -1,18 +1,68 @@
-import { useRouter } from 'next/router';
+import { NextPage } from 'next';
+import ErrorPage from 'next/error'
 import Layout from '../../components/Layout';
+import fetch from 'isomorphic-unfetch';
+import * as React from "react";
 
 interface IPlaceProps {
-    title: string;
-    address: string;
+    data?: {
+        name: string;
+        address: string;
+    },
+    error?: {
+        statusCode
+    }
 }
 
-export default function Place() {
-    const router = useRouter();
+const Place: NextPage<IPlaceProps> = ({error, data}) => {
+    if (error) {
+        return <ErrorPage statusCode={error.statusCode} />
+    } else {
+        return (
+            <Layout>
+                <div className="container">
+                    <div>
+                        <p>{data.name}</p>
+                        <p>{data.address}</p>
+                    </div>
+                    <div>
+                        <b>Opening hours</b>
+                    </div>
+                </div>
+                <style jsx>{`
+                    .container {
+                        display: flex;
+                        flex-direction: row;
+                        justify-content: space-between;
+                    }
+                `}</style>
+            </Layout>
+        );
+    }
+};
 
-    return (
-        <Layout>
-            <h1>{router.query.id}</h1>
-            <p>Place.....</p>
-        </Layout>
-    );
-}
+Place.getInitialProps = async ({ query }) => {
+    const { id } = query;
+    const res = await fetch(`http://localhost:8080/places/${id}`);
+    if (res.ok) {
+        const data = await res.json();
+
+        console.log(`Show data fetched. Count: ${data.length}`);
+
+        return {
+            data: {
+                name: data.name,
+                address: data.address
+            }
+        };
+    } else {
+        return {
+            error: {
+                statusCode: res.status
+            }
+        }
+    }
+
+};
+
+export default Place;
